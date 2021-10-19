9ddd28209c3c15749c0018d53fa7c5d914cb24cf
Suraj Doss <suraj_sebastien@yahoo.com>
2021-10-19 13:58:42 +0530

    Update 350. Intersection of Two Arrays II.cpp

--- 350. Intersection of Two Arrays II.cpp
+++ 350. Intersection of Two Arrays II.cpp
@@ -42,6 +42,7 @@ public:
         while(i<nums1.size() && j< nums2.size()){
             if(nums1[i] == nums2[j]){
                 ans.push_back(nums1[i]);
+                //ans.push_back(nums1[j]);
                 i++;
                 j++;
             }
