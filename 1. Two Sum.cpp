41f07e3c042db4c7c305d7d6308deffa1ed0a0ef
Suraj Doss <suraj_sebastien@yahoo.com>
2021-10-19 13:42:16 +0530

    Update 1. Two Sum.cpp

--- 1. Two Sum.cpp
+++ 1. Two Sum.cpp
@@ -40,9 +40,7 @@ public:
             //int ntf = target - nums[a];
             for(int b = a + 1; b< nums.size(); b++){
                 if(nums[a]+nums[b] == target){
-                    ans.push_back(a);
-                    ans.push_back(b);
-                    return ans;
+                     return {a,b};
                 }
             }
         }
