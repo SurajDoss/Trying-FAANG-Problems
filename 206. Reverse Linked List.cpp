49c1633394907e519785f53644c19fe528c18dd4
Suraj Doss <suraj_sebastien@yahoo.com>
2021-11-16 14:39:48 +0530

    Create 206. Reverse Linked List.cpp

---
+++ 206. Reverse Linked List.cpp
@@ -0,0 +1,44 @@
+/*
+Given the head of a singly linked list, reverse the list, and return the reversed list.
+
+ 
+
+Example 1:
+
+
+Input: head = [1,2,3,4,5]
+Output: [5,4,3,2,1]
+Example 2:
+
+
+Input: head = [1,2]
+Output: [2,1]
+Example 3:
+
+Input: head = []
+Output: []
+ 
+
+Constraints:
+
+The number of nodes in the list is the range [0, 5000].
+-5000 <= Node.val <= 5000
+https://leetcode.com/problems/reverse-linked-list/
+*/
+
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        ListNode* next;
+        ListNode* prev = NULL;
+        ListNode* cur = head;
+            
+        while(next){
+            next = cur->next;
+            cur->next = prev;
+            prev = cur;
+            cur = next;
+        }
+      return prev;  
+    }
+};
