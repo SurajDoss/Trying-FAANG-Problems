ec6f9b36356506e0c0a28d31a1b2f8c54fa8ef83
Suraj Doss <suraj_sebastien@yahoo.com>
2021-11-27 19:48:23 +0530

    Create 125. Valid Palindrome.cpp

---
+++ 125. Valid Palindrome.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        if(s.size()<1){
+            return true;
+        }
+        
+        for(int i = 0, j = s.size()-1; i<j; i++, j--){
+            while(isalnum(s[i])== false && i<j){
+                i++;
+            }
+            while(isalnum(s[j])== false && i<j){
+                j--;
+            }
+            if(toupper(s[i]) != toupper(s[j])){
+                return false;
+            }
+        }
+        return true;
+        
+    }
+};
