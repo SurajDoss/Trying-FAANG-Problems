6b0c90d71a663ce89edca13c2a879f1a1ed41e09
Suraj Doss <suraj_sebastien@yahoo.com>
2022-01-07 22:26:28 +0530

    Update 125. Valid Palindrome.cpp

--- 125. Valid Palindrome.cpp
+++ 125. Valid Palindrome.cpp
@@ -6,10 +6,10 @@ public:
         }
         
         for(int i = 0, j = s.size()-1; i<j; i++, j--){
-            while(isalnum(s[i])== false && i<j){
+            while(!isalnum(s[i]) && i<j){
                 i++;
             }
-            while(isalnum(s[j])== false && i<j){
+            while(!isalnum(s[j]) && i<j){
                 j--;
             }
             if(toupper(s[i]) != toupper(s[j])){
